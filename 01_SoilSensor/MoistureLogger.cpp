3f9802dc8fb06b91bf5671e67b510e59b78501e8
Tusori Tibor <ttuti94@gmail.com>
2020-12-20 21:33:41 +0100

    Add basic analog read code.

--- 01_SoilSensor/MoistureLogger.cpp
+++ 01_SoilSensor/MoistureLogger.cpp
@@ -3,12 +3,18 @@
 void setup()
 {
 	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(9600);
 }
 
 void loop()
 {
+	float l_sensorValue_f32 = analogRead(A0);
+
+	Serial.println(l_sensorValue_f32);
+
+	// signal the sample
 	digitalWrite(LED_BUILTIN, HIGH);
-	delay(1000);
+	delay(100);
 	digitalWrite(LED_BUILTIN, LOW);
 	delay(1000);
 }
